17c6971482b02ed6d551451ece41051b23f93993
kaustubh-24 <kaustubh.pathak@coupa.com>
2023-01-05 11:56:22 +0530

    add one_to_many association b/w user-article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,6 +16,7 @@ class ArticlesController < ApplicationController
 
     def create
         @article = Article.new(params.require(:article).permit(:title, :description))
+        @article.user = User.first
         if @article.save
             flash[:notice] = "Article created successfully!"
             redirect_to @article
